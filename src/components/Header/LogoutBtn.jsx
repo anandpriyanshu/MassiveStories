489d3ab273415a48b4936441da2e7902dea06cf8
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-09-29 11:47:00 +0530

    Create LogoutBtn compnent & Implement Navigation logic in Header component

---
+++ src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/auth'
+import { logout } from '../../store/AuthSlice'
+
+const LogoutBtn = () => {
+
+    const logOutHandler = () => {
+
+
+        authService.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+
+    const dispatch = useDispatch()
+    return (
+        <>
+
+            <button onClick={logOutHandler} className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Log out</button>
+
+        </>
+    )
+}
+
+export default LogoutBtn
