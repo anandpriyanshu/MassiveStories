40c37e538dacdac5eb2b53a8b7ef22d447a6a941
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-10-02 12:09:44 +0530

    Create Login component. Design Form using React hook form. Import Input in Indexjs

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from './Input'
 
 
-
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Input };
