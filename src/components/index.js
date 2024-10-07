2f4bf6aa78a0eb5902b41030b462a020a1a21bad
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-10-07 16:45:59 +0530

    Create Login and Signup component in pages. Import both in indexJS

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,10 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from './Input'
 import Select from './Select'
+import Signup from "./Signup";
+import Login from "./Login";
 
 
 import ReacttimeEditor from './ReactImeEditor'
 
-export { Header, Footer, Container, Logo, LogoutBtn, Input, ReacttimeEditor, Select };
+export { Header, Footer, Container, Logo, LogoutBtn, Input, ReacttimeEditor, Select, Signup, Login };
